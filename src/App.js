60ec0a436d07ad322aba062d9c88f68c47718726
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-09-10 03:06:14 -0300

    Adding a simple navigation with two links => One leading to "Users", one leading to "Courses"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
@@ -19,6 +19,10 @@ class App extends Component {
             <li>Add a 404 error page and render it for any unknown routes</li>
             <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
           </ol>
+          <nav>
+            <NavLink to="/courses"><button>Courses</button></NavLink>
+            <NavLink to="/users"><button>Users</button></NavLink>
+          </nav>
           <Route path="/courses" component={Courses} />
           <Route path="/users" component={Users} />
         </div>
