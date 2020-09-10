092519d375d07f92e2fcec8108128127031547b8
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-09-10 19:14:51 -0300

    Passing the course ID to the "Course" page and outputting it there

--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -20,7 +20,7 @@ class Courses extends Component {
                     {
                         this.state.courses.map( course => {
                             return (
-                                <Link key={course.id} to={this.props.match.url + '/course'}>
+                                <Link key={course.id} to={this.props.match.url + '/' + course.id}>
                                     <article className="Course" key={course.id}>{course.title}</article>
                                 </Link>
                             );
